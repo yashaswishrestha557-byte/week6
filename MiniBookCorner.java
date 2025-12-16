b4c95d2bbe17b44f172b880b44bb407a1214c7c0
yashaswi <yashaswishrestha557@gmail.com>
2025-12-16 23:06:48 +0545

    update

--- MiniBookCorner.java
+++ MiniBookCorner.java
@@ -8,20 +8,20 @@
 public class MiniBookCorner {
     public static void main(String[] args) {
         String[] categories = {"Fiction", "Nepali"};
-        String[][] title = new String[2][1];
-        double[][] price = new double[2][1];
+        String[][] title=new String[2][1];
+        double[][] price=new double[2][1];
 
-        title[0][0] = "Asahamati - 5";
-        price[0][0] = 750.0;
+        title[0][0]="Asahamati - 5";
+        price[0][0]=750.0;
 
-        title[1][0] = "Muna Madan";
-        price[1][0] = 500.0;
+        title[1][0]="Nepali floklore";
+        price[1][0]=500.0;
 
         for (int i = 0; i < categories.length; i++) {
             System.out.println("Category: " + categories[i]);
-            for (int j=0; j<1; j++) {
+            for (int j=0; j<1; j++){
                 System.out.println("Title:"+title[i][j]);
-                System.out.println("Price"+price[i][j]);
+                System.out.println("Price:"+price[i][j]);
             }
         }
     }
